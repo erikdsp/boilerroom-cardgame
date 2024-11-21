de9cb7c5aa8dc9116c1e6c6019c6e44004e80d8d
Erik Dahl <erik@iunderlandet.se>
2024-11-21 19:26:21 +0100

    Clean up main

--- main.cpp
+++ main.cpp
@@ -1,66 +1,27 @@
-#include "Dealer.h"
+#include "Deck.h"
 #include <iostream>
 
-// testing. Moved to CardDealer
-// bool oh_another_card(Deal& d);
 
 int main()
 {
 
-// Don't need to be a class
+// Don't need to be a class. But maybe can be for now
 RandomGenerator gen{};
-
-// Dont need to be a class either
-// CardDealer bj_dealer{};
 
 /**
  * TODO: Implement Card Dealer in main 
  */
 
 /**   
- * TODO: New data structure for Deck 
+ * DONE: New data structure for Deck 
+ * TODO: Functions that loops through card vector
  * PROS: more robust data integrity = less room for bugs
  * CONS: requires more vector looping = slower
 */
 
 
-// Deal player1{};
-// Deal player2{};
-// 
-// while (bj_dealer.game_on())
-// {
-//     if (!player1.playing())
-//     {
-//         if (bj_dealer.another_card(player1))    // passing reference changes player1
-//         {
-//             bj_dealer.deal(player1);            // passing reference changes player1
-//         }
-//     }
 
-//     if (!player2.playing())
-//     {
-//         if (bj_dealer.another_card(player2))
-//         {
-//             player2.add_card(bj_dealer.draw_card(player2.get_deal()));  // old version that sends Cards and Deals
-//         }
-//     }
-// }
 
 return 0;
 }
 
-
-// bool oh_another_card(Deal& d)  // passing a reference might change d
-// {
-//     char y_or_n;
-//     std::cin >> y_or_n;     // gör while loop
-//     if (y_or_n == 'y')
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         d.stand();
-//         return false;
-//     }
-// }
